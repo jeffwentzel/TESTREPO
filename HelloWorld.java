e52c4f1a0d556bd96c98aefeea1de21cf75401af
jeffwentzel <42041288+jeffwentzel@users.noreply.github.com>
2020-04-29 17:23:47 -0400

    BRT-605 update again

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 /* @wi.mainClass BRT-605	Sample java program
-	FileName: "helloWorld.java".	*/
+FileName: "helloWorld.java". */
 class HelloWorld
 {	
 	public static void main(String args[])
 	{
-		/* @wi.command BRT-605 words */
+		/* @wi.command BRT-605 words
 		System.out.println("Hola");
 	}
 }
