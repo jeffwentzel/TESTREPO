b503cfd4ddbaabd380c73812fec2a5ee318db5ae
jeffwentzel <42041288+jeffwentzel@users.noreply.github.com>
2020-04-29 17:18:14 -0400

    BRT-605 updating comments to review linked resrc

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
-/*	Sample java program
+/ *@wi.mainClass BRT-605	Sample java program
 	FileName: "helloWorld.java".	*/
 class HelloWorld
 {	
 	public static void main(String args[])
 	{
+		/* @wi.command BRT-605 words */
 		System.out.println("Hola");
 	}
 }
